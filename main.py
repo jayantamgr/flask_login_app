bbda50db45467d47e0225e1e7dd37af1b679a85d
jayantamgr <jayantamgr@gmail.com>
2020-01-19 15:47:43 +0100

    edited main.html

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -25,9 +26,10 @@ def login():
 		username = request.form['username']
 		password = request.form['password']
 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+		cursor.execute('SELECT * FROM accounts WHERE username = %s', [username])
 		account = cursor.fetchone()
-		if account:
+		print(account['password'])
+		if account and check_password_hash(account['password'], password):
 			session['loggedin'] = True
 			session['id'] = account['id']
 			session['username'] = account['username']
@@ -46,11 +48,18 @@ def logout():
 
 @app.route('/login/register/', methods = ['GET', 'POST'])
 def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+	#msg = ''
+	new_account_info = None
+	if request.method == 'POST':# and 'firstname' in request.form and 'birthdate' in request.form and 'password' in request.form and 'email' in request.form:
+		
+		firstname = request.form['firstname'] 
+		lastname = request.form['lastname']
 		username = request.form['username']
 		password = request.form['password']
 		email = request.form['email']
+		birthdate = request.form['birthdate']
+		nationality = request.form['nationality']
+		salutation = request.form['salutation']
 
 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 		cursor.execute('SELECT * FROM accounts WHERE username = %s', [username])
@@ -64,13 +73,19 @@ def register():
 		elif not username or not password or not email:
 		    msg = 'Please fill out the form'
 		else:
-		    cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
-		    mysql.connection.commit()
-		    msg = 'You have successfully registered'    	
-
+			passw = generate_password_hash(password)
+			cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, passw, email))
+			mysql.connection.commit()
+			last_id = cursor.lastrowid
+			cursor.execute('INSERT INTO user VALUES (NULL, %s, %s, %s, %s, %s, %s)', (last_id, salutation, firstname, lastname, birthdate, nationality))
+			mysql.connection.commit()
+			cursor.execute('SELECT * FROM accounts JOIN user WHERE user.account_id = %s AND accounts.id = %s', [last_id, last_id])
+			#msg = 'You have successfully registered'    	
+			new_account_info = cursor.fetchone()
+			return render_template('welcome.html', new_account_info=new_account_info)	
 	elif request.method == 'POST':
 		msg = 'Please fill out the form'
-	return render_template('register.html', msg=msg)	
+	return render_template('register.html')
 
 
 @app.route('/login/home')
@@ -85,9 +100,24 @@ def profile():
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        cursor.execute('SELECT * FROM accounts JOIN user WHERE user.account_id = %s AND accounts.id = %s', [session['id'], session['id']])
         account = cursor.fetchone()
         # Show the profile page with account info
         return render_template('profile.html', account=account)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+@app.route('/pythonlogin/profile/editprofile', methods = ['GET', 'POST', 'PUT'])
+def editprofile():
+	return render_template('editprofile.html')
+	"""
+	if 'loggedin' in session:
+		firstname = request.form['firstname'] 
+		lastname = request.form['lastname']
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		birthdate = request.form['birthdate']
+		nationality = request.form['nationality']
+		salutation = request.form['salutation']
+	"""
